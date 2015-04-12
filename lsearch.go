aaadcf3aa4108a38bdfc8abade03c8caffaa4de2
yqingp <yqingp@gmail.com>
2015-04-12 16:39:21 +0800

    update structure

--- lsearch.go
+++ lsearch.go
@@ -1,28 +1,28 @@
-package lsearch
+package main
 
 // "github.com/yqingp/lsearch/"
 
 import (
-	"fmt"
-	"github.com/yqingp/lsearch/index"
+    "fmt"
+    "github.com/yqingp/lsearch/index"
 )
 
 type LSearch struct {
-	config *config
-	engine *index.Engine
+    config *config
+    engine *index.Engine
 }
 
 func NewLSearch(configFilePath string) *LSearch {
-	return &LSearch{
-		config: newConfig(configFilePath),
-		engine: &index.Engine{},
-	}
+    return &LSearch{
+        config: newConfig(configFilePath),
+        engine: &index.Engine{},
+    }
 }
 
 func (this *LSearch) Init() {
-	fmt.Println("init")
-	this.config.initStorePath()
-	if err := this.engine.Init(this.config.storePath); err != nil {
-		Logger.Fatal(err)
-	}
+    fmt.Println("init")
+    this.config.initStorePath()
+    if err := this.engine.Init(this.config.storePath); err != nil {
+        Logger.Fatal(err)
+    }
 }

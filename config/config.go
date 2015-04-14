4401c642b07bfacbe087ca7f3ead1678fee21d98
jack.qin <qinjker@gmail.com>
2015-04-14 23:47:12 +0800

    real config

--- config/config.go
+++ config/config.go
@@ -1,20 +1,30 @@
 package config
 
-import ()
+import (
+	"encoding/json"
+	"os"
+)
 
 const (
-    DefaultStorePath = "lsearch_store"
+	DefaultStorePath = "lsearch_store"
 )
 
 type Config struct {
-    StorePath string `json:"store_path"`
-    IP        string
-    Port      string
+	StorePath string `json:"store_path"`
+	IP        string
+	Port      string
 }
 
 // parse the root dir "config.json" and init base store path
 // if basePath is nil  then use default file path `DefaultStorePath`
 func NewConfig() *Config {
-
-    return nil
+	configFile, err := os.Open("../config.json")
+	if err != nil {
+		//Logger.Fatal(err.Error())
+	}
+	jsonParser := json.NewDecoder(configFile)
+	if err = jsonParser.Decode(&Config); err != nil {
+		//printError("parsing config file", err.Error())
+	}
+	return nil
 }

2d42791330936a2faf8ea6ffe0dd266da6fb05e1
jack.qin <qinjker@gmail.com>
2015-04-15 16:33:04 +0800

    up

--- config/config.go
+++ config/config.go
@@ -1,41 +1,43 @@
 package config
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "log"
-    "os"
-    "path/filepath"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 )
 
 const (
-    DefaultStorePath = "lsearch_store"
+	DefaultStorePath = "lsearch_store"
+	Ip               = "127.0.0.1"
+	Port             = "9919"
 )
 
 // define default ip and port
 
 type Config struct {
-    StorePath string `json:"store_path"`
-    IP        string
-    Port      string
+	StorePath string `json:"store_path"`
+	Ip        string
+	Port      string
 }
 
 // parse the root dir "config.json" and init base store path
 // if basePath is nil  then use default file path `DefaultStorePath`
 // using default config "store" or "ip" or "port" if config.json not exist
 func NewConfig() *Config {
-    config_st := &Config{}
-    content, err := ioutil.ReadFile("config.json")
-    if err != nil {
-        log.Fatal(err)
-    }
-    //json.Unmarshal(, v)
-    err = json.Unmarshal(content, config_st)
-    if err != nil {
-        log.Fatal(err)
-    }
-    filepath.Abs(os.Args[0])
-    log.Printf("%v", config_st.Port)
+	str := `{"store_path":"` + DefaultStorePath + `",` + `"ip":"` + Ip + `",` + `"port":"` + Port + `"` + `}`
+	config := &Config{}
+	json.Unmarshal([]byte(str), config)
+	//log.Printf("%v", str)
+	content, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return config_st
+	err = json.Unmarshal(content, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%v", config.Port)
+	return config
 }

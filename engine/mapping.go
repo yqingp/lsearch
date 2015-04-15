c6ea90dcf9fae6af8355d125ee02060bbfb0e487
yqingp <yqingp@gmail.com>
2015-04-15 16:50:18 +0800

    update mapping and add comments

--- engine/mapping.go
+++ engine/mapping.go
@@ -3,6 +3,20 @@ package engine
 type MappingRequest struct {
 }
 
+/*
+post structure
+{
+	action: "update||create||delete",
+	name:"abc"
+	fields:[
+	{:name => "id"}unique
+	{:name => fields1, :type => (string|text), :analyzer => xxx}
+	...
+	...
+	]
+}
+
+*/
 func (e *Engine) MappingHandler(action string, body []byte) {
 
 }

1469f8c7c9d9d05b772e509e58608407b01da045
yqingp <yqingp@gmail.com>
2015-04-24 14:26:27 +0800

    update

--- lsearch.go
+++ lsearch.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "encoding/json"
+    "github.com/gin-gonic/gin"
     "github.com/yqingp/lsearch/engine"
     "io/ioutil"
     "log"
@@ -9,7 +10,9 @@ import (
     "runtime"
 )
 
-var lsearch engine.Engine
+var LSearch engine.Engine
+
+var Router *gin.Engine
 
 type ResponseResult struct {
     Results      interface{} `json:"results"`
@@ -22,7 +25,7 @@ func mapping(rw http.ResponseWriter, req *http.Request) {
     defer req.Body.Close()
 
     result := ResponseResult{}
-    results, err := lsearch.MappingHandler(body)
+    results, err := LSearch.MappingHandler(body)
     if err != nil {
         result.Response = err.Error()
         result.ResponseCode = 500
@@ -61,7 +64,7 @@ func viewIndexHandler(rw http.ResponseWriter, req *http.Request) {
     name := req.URL.Query()["name"][0]
     result := ResponseResult{}
 
-    results, err := lsearch.ViewIndex(name)
+    results, err := LSearch.ViewIndex(name)
     if err != nil {
         result.Response = err.Error()
         result.ResponseCode = 500
@@ -82,23 +85,100 @@ func indexDocumentsHandler(rw http.ResponseWriter, req *http.Request) {
     body, _ := ioutil.ReadAll(req.Body)
     defer req.Body.Close()
 
-    results := lsearch.Index(body)
+    results := LSearch.Index(body)
     w, _ := json.Marshal(results)
     rw.WriteHeader(200)
     rw.Write(w)
 }
 
-func routes() {
-    http.HandleFunc("/_mapping", mapping)
-    http.HandleFunc("/_status/", statusHandler)
-    http.HandleFunc("/search", searchHandler)
-    http.HandleFunc("/index/view", viewIndexHandler)
-    http.HandleFunc("/index/documents", indexDocumentsHandler)
+func Routes() {
+    // 创建索引
+    Router.POST("/:name", func(c *gin.Context) {
+
+    })
+
+    // 更新索引
+    Router.PUT("/:name", func(c *gin.Context) {
+
+    })
+
+    // 删除索引
+    Router.DELETE("/:name", func(c *gin.Context) {
+
+    })
+
+    // 查看索引数据
+    Router.GET("/:name", func(c *gin.Context) {
+
+    })
+
+    // 查看索引结构
+    Router.GET("/:name/_mapping", func(c *gin.Context) {
+
+    })
+
+    // 查看索引状态统计
+    Router.GET("/:name/_status", func(c *gin.Context) {
+
+    })
+
+    // 根据ID同时查询多条数据 多个索引下的数据
+    //     "docs" : [
+    //     {
+    //         "index" : "test",
+    //         "id" : "1"
+    //     },
+    //     {
+    //         "index" : "test",
+    //         "id" : "2"
+    //     }
+    // ]
+    Router.GET("/_mget", func(c *gin.Context) {
+
+    })
+
+    // 某个索引下面的多个数据根据ID查询
+    // "ids" : ["1", "2"]
+    Router.GET("/:name/_mget", func(c *gin.Context) {
+
+    })
+
+    // 查看索引数据 单条
+    Router.GET("/:name/:id", func(c *gin.Context) {
+
+    })
+
+    // 查看删除单挑数据
+    Router.DELETE("/:name/:id", func(c *gin.Context) {
+
+    })
+
+    // 查看更新单条素据或者插入
+    Router.PUT("/:name/:id", func(c *gin.Context) {
+
+    })
+
+    //插入数据
+    Router.POST("/:name/:id", func(c *gin.Context) {
+
+    })
+
+    // 批量操作
+    Router.POST("/_bulk", func(c *gin.Context) {
+
+    })
+
+    // http.HandleFunc("/_mapping", mapping)
+    // http.HandleFunc("/_status/", statusHandler)
+    // http.HandleFunc("/search", searchHandler)
+    // http.HandleFunc("/index/view", viewIndexHandler)
+    // http.HandleFunc("/index/documents", indexDocumentsHandler)
 }
 
 func main() {
-    lsearch.Init()
+    Router = gin.Default()
+    LSearch.Init()
     runtime.GOMAXPROCS(runtime.NumCPU())
-    routes()
-    http.ListenAndServe(lsearch.BindAddr(), nil)
+    Routes()
+    Router.Run(LSearch.BindAddr())
 }
